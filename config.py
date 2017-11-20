93766b76f2f9c152ea8e848a737289c6d9434b66
rzhon <rzwei@foxmail.com>
2017-11-20 23:57:53 +0800

    modify config.py

--- config.py
+++ config.py
@@ -9,18 +9,20 @@ addfriendjson = './myJson/AddFriend.json'
 waitpicjson = './myJson/PictureNoReply.json'
 dailycheckjson = './myJson/dailycheck.json'
 afterTarotjson = './myJson/afterTarot.json'
+imagepath = './images/'
+picname = ['a.png', 'b.png', 'c.png']
 
-host="127.0.0.1"
-port='5000'
-account='admin'
-password="admin"
-alvieCount=1
+host = "127.0.0.1"
+port = '5000'
+account = 'admin'
+password = "admin2"
+alvieCount = 1
 d = None
 try:
     with open('config.json', encoding='utf-8') as fin:
         d = json.load(fin)
 except Exception as e:
-    print('open config.json error',e)
+    print('open config.json error', e)
 if d:
     if 'dbaddress' in d:
         dbaddress = d['dbaddress']
@@ -43,10 +45,14 @@ if d:
     if 'account' in d:
         account = d['account']
     if 'password' in d:
-        account = d['password']
+        password = d['password']
     if 'host' in d:
         host = d['host']
     if 'port' in d:
         port = d['port']
     if 'aliveCount' in d:
         aliveCount = d['aliveCount']
+    if 'imagepath' in d:
+        imagepath = d['imagepath']
+    if 'picname' in d:
+        picname = d['picname']
